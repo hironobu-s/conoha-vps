903aed082c7ba94b2e1b164cfd60191b3d4886a1
Hironobu <hiro@hironobu.org>
2015-01-31 22:08:46 +0900

    出力フォーマットが正しく反映されていなかった

--- lib/log.go
+++ lib/log.go
@@ -1,8 +1,11 @@
 package lib
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/Sirupsen/logrus"
 	"os"
+	"sort"
 )
 
 func init() {
@@ -13,8 +16,7 @@ var instance *logrus.Logger
 func GetLogInstance() *logrus.Logger {
 	if instance == nil {
 
-		// テキストフォーマットで出力
-		logrus.SetFormatter(&logrus.TextFormatter{})
+		//logrus.SetFormatter(&logrus.TextFormatter{})
 		//logrus.SetFormatter(&logrus.JSONFormatter{})
 
 		// 標準エラー出力に出す
@@ -22,9 +24,42 @@ func GetLogInstance() *logrus.Logger {
 
 		// ログレベルの設定
 		instance = logrus.New()
+
 		instance.Level = logrus.DebugLevel
+		//instance.Formatter = &SimpleFormatter{}
+		instance.Formatter = &logrus.TextFormatter{}
+
 		//instance.Level = logrus.InfoLevel
 		//instance.SetOutput(os.Stderr)
 	}
 	return instance
+}
+
+type SimpleFormatter struct {
+}
+
+func (f *SimpleFormatter) appendKeyValue(b *bytes.Buffer, key, value interface{}) {
+	switch value.(type) {
+	case string, error:
+		fmt.Fprintf(b, "%v=%q ", key, value)
+	default:
+		fmt.Fprintf(b, "%v=%v ", key, value)
+	}
+}
+
+func (f *SimpleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+
+	var keys []string
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	b := &bytes.Buffer{}
+
+	fmt.Fprintf(b, "%s", entry.Message)
+
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+
 }
