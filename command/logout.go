fbe56ca738ee587f15c97d23cf651bdac23628f7
Hironobu <hiro@hironobu.org>
2015-02-01 19:59:05 +0900

    Usage()を実装

--- command/logout.go
+++ command/logout.go
@@ -1,5 +1,12 @@
 package command
 
+import (
+	"errors"
+	"fmt"
+	flag "github.com/ogier/pflag"
+	"os"
+)
+
 type Logout struct {
 	*Command
 }
@@ -11,10 +18,39 @@ func NewLogout() *Logout {
 }
 
 func (cmd *Logout) parseFlag() error {
+	var help bool
+
+	fs := flag.NewFlagSet("conoha-vps", flag.ContinueOnError)
+	fs.Usage = cmd.Usage
+
+	fs.BoolVarP(&help, "help", "h", false, "help")
+
+	fs.Parse(os.Args[1:])
+
+	if help {
+		fs.Usage()
+		return errors.New("")
+	}
+
 	return nil
 }
 
+func (cmd *Logout) Usage() {
+	fmt.Println(`Usage: conoha logout [OPTIONS ...]
+
+DESCRIPTION
+    Remove an authenticate file(~/.conoha-vps).
+
+OPTIONS
+    -h: --help:     Show usage.      
+`)
+}
+
 func (cmd *Logout) Run() error {
+	if err := cmd.parseFlag(); err != nil {
+		return err
+	}
+
 	cmd.config.Remove()
 	return nil
 }
