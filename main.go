19476a2875ae2caf6e919f9637cb279f4e63a5bf
Hironobu <hiro@hironobu.org>
2015-02-01 23:17:14 +0900

    initial commit.

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"github.com/hironobu-s/conoha-vps/command"
+	"github.com/hironobu-s/conoha-vps/lib"
+	"os"
+)
+
+func main() {
+
+	log := lib.GetLogInstance()
+
+	var cmd command.Commander
+	var subcommand string = ""
+
+	if len(os.Args) > 1 {
+		subcommand = os.Args[1]
+	}
+
+	log.Debugf("starting %s subcommand...", subcommand)
+
+	switch subcommand {
+	case "login":
+		cmd = command.NewLogin()
+	case "stat":
+		cmd = command.NewVpsStat()
+	case "list":
+		cmd = command.NewVpsList()
+	case "add":
+		cmd = command.NewVpsAdd()
+	case "remove":
+		cmd = command.NewVpsRemove()
+	case "ssh-key":
+		cmd = command.NewSshKey()
+	case "ssh":
+		cmd = command.NewSsh()
+	case "logout":
+		cmd = command.NewLogout()
+	case "version":
+		cmd = command.NewVersion()
+	default:
+		cmd = command.NewNocommand()
+	}
+	defer cmd.Shutdown()
+
+	// login, logout, version, nocommand以外はログインが必須
+	_, nocommand := cmd.(*command.Nocommand)
+	if subcommand != "login" && subcommand != "version" && subcommand != "logout" && !nocommand {
+		l := command.NewLogin()
+
+		loggedIn, _ := l.LoggedIn()
+		if !loggedIn {
+			log.Errorf("Session is timed out. Please log in.")
+			return
+		}
+	}
+
+	if err := cmd.Run(); err != nil {
+		// ShowUsageErrorの場合はUsage()を表示してるだけなのでログは表示しない
+		_, ok := err.(*command.ShowUsageError)
+		if !ok {
+			log.Error(err)
+		}
+	}
+}
