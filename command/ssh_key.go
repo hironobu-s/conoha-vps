01d54b84565ab6ae3d3a380141ee89044cfcb244
Hironobu <hiro@hironobu.org>
2015-02-07 22:05:36 +0900

    SSHキーが複数ある場合に、指定してダウンロードできるようにした

--- command/ssh_key.go
+++ command/ssh_key.go
@@ -1,7 +1,9 @@
 package command
 
 import (
+	"errors"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/hironobu-s/conoha-vps/cpanel"
 	"github.com/hironobu-s/conoha-vps/lib"
 	flag "github.com/ogier/pflag"
@@ -10,6 +12,8 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 type PrivateKey string
@@ -17,6 +21,7 @@ type PrivateKey string
 type SshKey struct {
 	*Vps
 	destPath string
+	sshKeyNo int
 }
 
 func NewSshKey() *SshKey {
@@ -33,6 +38,7 @@ func (cmd *SshKey) parseFlag() error {
 
 	fs.BoolVarP(&help, "help", "h", false, "help")
 	fs.StringVarP(&cmd.destPath, "path", "f", "", ``)
+	fs.IntVarP(&cmd.sshKeyNo, "sshkey-no", "s", 1, ``)
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		fs.Usage()
@@ -46,7 +52,7 @@ func (cmd *SshKey) parseFlag() error {
 
 	if cmd.destPath == "" {
 		// デフォルト
-		cmd.destPath = "conoha-" + cmd.config.Account + ".key"
+		cmd.destPath = "conoha-" + cmd.config.Account + "-" + strconv.Itoa(cmd.sshKeyNo) + ".key"
 	}
 
 	return nil
@@ -60,10 +66,13 @@ DESCRIPTION
 
 
 OPTIONS
-    -f: --file:  Local filename the private key is stored.
-                 Default is "conoha-{AccountID}.key"
+    -f: --file:       Local filename the private key is stored.
+                      Default is "conoha-{AccountID}.key"
 
-    -h: --help:  Show usage.      
+    -s: --sshkey-no:  SSH Key number. Default is 1.
+                      If the number of keys one, It wil be ignored.
+
+    -h: --help:       Show usage.      
 `)
 }
 
@@ -85,7 +94,7 @@ func (cmd *SshKey) Run() error {
 }
 
 // SSH秘密鍵をダウンロードする
-func (cmd *Vps) DownloadSshKey(destPath string) error {
+func (cmd *SshKey) DownloadSshKey(destPath string) error {
 	var err error
 	destPath, err = filepath.Abs(destPath)
 	if err != nil {
@@ -113,46 +122,88 @@ func (cmd *Vps) DownloadSshKey(destPath string) error {
 }
 
 // SSH秘密鍵を取得する
-func (cmd *Vps) SshKey() (PrivateKey, error) {
-
-	var vm *Vm
+func (cmd *SshKey) SshKey() (PrivateKey, error) {
 	var err error
-
-	vpsList := NewVpsList()
-	servers, err := vpsList.List(false)
-	if err != nil {
-		return "", err
-	}
-
-	for _, v := range servers { // これ他に良いやり方無いかな？？？
-		vm = v
-		break
-	}
 
 	var act *cpanel.Action
 
-	rt := &sshDownloadKeyResult{}
+	// 秘密鍵一覧ページを取得して鍵の一覧を取得する
+	rt := &sshDownloadFormResult{
+		sshKeyNo: cmd.sshKeyNo,
+	}
+	act = &cpanel.Action{
+		Request: &sshDownloadFormRequest{},
+		Result:  rt,
+	}
+	cmd.browser.AddAction(act)
+
+	rtd := &sshDownloadKeyResult{}
 	act = &cpanel.Action{
 		Request: &sshDownloadKeyRequest{
-			vm: vm,
+			formResult: rt,
 		},
-		Result: rt,
+		Result: rtd,
 	}
 	cmd.browser.AddAction(act)
 
 	if err = cmd.browser.Run(); err != nil {
 		return "", err
 	}
+	return rtd.SshKey, nil
+}
 
-	return rt.SshKey, nil
+type sshDownloadFormRequest struct {
+}
+
+func (r *sshDownloadFormRequest) NewRequest(values url.Values) (*http.Request, error) {
+	return http.NewRequest("GET", "https://cp.conoha.jp/Service/VPS/keyPair/", nil)
+}
+
+type sshDownloadFormResult struct {
+	sshKeyNo   int
+	SshKeyName string
+}
+
+func (r *sshDownloadFormResult) Populate(resp *http.Response, doc *goquery.Document) error {
+	var sel *goquery.Selection
+	sel = doc.Find("#ContentPlaceHolder1_ContentPlaceHolder1_gridSSHKeyList .btnIconPrivateKeyDL02")
+
+	i := 0
+	for n := range sel.Nodes {
+		if i != r.sshKeyNo-1 {
+			i++
+			continue
+		}
+
+		node := sel.Eq(n)
+		name, exists := node.Attr("name")
+		if exists {
+			r.SshKeyName = name
+		}
+		break
+	}
+
+	if r.SshKeyName == "" {
+		return errors.New("SSH Key not found.")
+	}
+
+	return nil
 }
 
 type sshDownloadKeyRequest struct {
-	vm *Vm
+	formResult *sshDownloadFormResult
 }
 
 func (r *sshDownloadKeyRequest) NewRequest(values url.Values) (*http.Request, error) {
-	return http.NewRequest("GET", "https://cp.conoha.jp/Service/VPS/Control/DownloadPrivateKey/"+r.vm.Id, nil)
+	values.Add(r.formResult.SshKeyName, "Private Key Download")
+	values.Add("ctl00$ctl00$ContentPlaceHolder1$ContentPlaceHolder1$hfTargetKey", "")
+
+	req, err := http.NewRequest("POST", "https://cp.conoha.jp/Service/VPS/keyPair/", strings.NewReader(values.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	return req, nil
 }
 
 type sshDownloadKeyResult struct {
